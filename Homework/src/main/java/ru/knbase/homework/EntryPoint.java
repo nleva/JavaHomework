ee4b754e6b0a3274bd8420cb081197db1a96528f
nleva <n.lev.a@ya.ru>
2017-02-01 15:00:42 +0300

    Empty maven,eclipse project

---
+++ Homework/src/main/java/ru/knbase/homework/EntryPoint.java
@@ -0,0 +1,20 @@
+package ru.knbase.homework;
+
+/**
+ * Точка входа для выполнения домашней работы
+ * @author Lev Nadeinsky
+ * @date	2017-02-01
+ */
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		// ----------> your code starts here <------------
+	}
+	
+	/*
+	 * приватный конструктор для
+	 * запрета создания экземпляра или наследования
+	 */
+	private EntryPoint() {}
+
+}
